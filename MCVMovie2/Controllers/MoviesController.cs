9e1abfa46b88aced142d2324b289354b7771da00
matthewmorris41 <matthewmorris41@gmail.com>
2015-07-22 11:46:21 -0400

    Migrations and seeding
    end of part 8 start of part 9

--- MCVMovie2/Controllers/MoviesController.cs
+++ MCVMovie2/Controllers/MoviesController.cs
@@ -15,10 +15,32 @@ namespace MCVMovie2.Controllers
         private MovieDBContext db = new MovieDBContext();
 
         // GET: Movies
-        public ActionResult Index()
-        {
-            return View(db.Movies.ToList());
-        }
+        public ActionResult Index(string movieGenre, string searchString)
+{
+    var GenreLst = new List<string>();
+
+    var GenreQry = from d in db.Movies
+                   orderby d.Genre
+                   select d.Genre;
+
+    GenreLst.AddRange(GenreQry.Distinct());
+    ViewBag.movieGenre = new SelectList(GenreLst);
+
+    var movies = from m in db.Movies
+                 select m;
+
+    if (!String.IsNullOrEmpty(searchString))
+    {
+        movies = movies.Where(s => s.Title.Contains(searchString));
+    }
+
+    if (!string.IsNullOrEmpty(movieGenre))
+    {
+        movies = movies.Where(x => x.Genre == movieGenre);
+    }
+
+    return View(movies);
+}
 
         // GET: Movies/Details/5
         public ActionResult Details(int? id)
--- MCVMovie2/Models/Movie.cs
+++ MCVMovie2/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.Web;
@@ -11,6 +12,8 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
